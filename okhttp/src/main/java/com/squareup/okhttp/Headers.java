1184893257/okhttp

commit 8ef2daccd20f80b439c93af7d7985fb26d1f6b2f

Workaround for request header values with trailing '\r' or '\r\n'
Testing is showing an app that appears to
be setting a request header with a trailing '\r'. This change
strips the '\r' or '\r\n' iff it is at the end of the value.

Bug: 26889631
Change-Id: I3a521a5272ad0c7cf3b018892516ced9e65f46ca

okhttp/src/main/java/com/squareup/okhttp/Headers.java
@@ -290,10 +290,15 @@ public final class Headers {
       }
       if (value == null) throw new IllegalArgumentException("value == null");
 
-      // Workaround for applications that set trailing "\n" on header values.
-      // http://b/26422335. Android used to allow anything except '\0'.
-      if (value.length() > 0 && value.charAt(value.length() - 1) == '\n') {
-        value = value.substring(0, value.length() - 1);
+      // Workaround for applications that set trailing "\r", "\n" or "\r\n" on header values.
+      // http://b/26422335, http://b/26889631 Android used to allow anything except '\0'.
+      int valueLen = value.length();
+      if (valueLen >= 2 && value.charAt(valueLen - 2) == '\r' && value.charAt(valueLen - 1) == '\n') {
+        value = value.substring(0, value.length() - 2);
+      } else if (valueLen > 0
+              && (value.charAt(valueLen - 1) == '\n'
+                      || value.charAt(valueLen - 1) == '\r')) {
+        value = value.substring(0, valueLen - 1);
       }
       // End of workaround.
 

okhttp-tests/src/test/java/com/squareup/okhttp/RequestTest.java
@@ -182,6 +182,8 @@ public final class RequestTest {
     // assertForbiddenHeader("\n");
     assertForbiddenHeader("a\nb");
     assertForbiddenHeader("\nb");
+    assertForbiddenHeader("a\rb");
+    assertForbiddenHeader("\rb");
     // End of Android modification.
     assertForbiddenHeader("\t");
     assertForbiddenHeader("\u001f");

commit 29c74f3e44860e0a31ef3b1f157e994134b31809

Fix long line: Workaround for request header values with trailing '\r' or '\r\n'
Commit 8ef2daccd20f80b439c93af7d7985fb26d1f6b2f introduced a long line
(101 characters) that broke the checkstyle pass of okhttp's internal
tests run via "mvn clean verify".

This CL wraps the line.

Test: mvn clean verify

Change-Id: I0074cb35908589d984b77f66bb11d02d414b42c8

okhttp/src/main/java/com/squareup/okhttp/Headers.java
@@ -293,7 +293,8 @@ public final class Headers {
       // Workaround for applications that set trailing "\r", "\n" or "\r\n" on header values.
       // http://b/26422335, http://b/26889631 Android used to allow anything except '\0'.
       int valueLen = value.length();
-      if (valueLen >= 2 && value.charAt(valueLen - 2) == '\r' && value.charAt(valueLen - 1) == '\n') {
+      if (valueLen >= 2 && value.charAt(valueLen - 2) == '\r'
+          && value.charAt(valueLen - 1) == '\n') {
         value = value.substring(0, value.length() - 2);
       } else if (valueLen > 0
               && (value.charAt(valueLen - 1) == '\n'